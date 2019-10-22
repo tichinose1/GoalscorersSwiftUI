1fbff580df1383c3f5de8acb72fd73ed2db43f29
tichinose1 <tichinose1dev@gmail.com>
2019-10-22 23:59:36 +0900

    Add MapView

---
+++ Goalscorers/Views/MapView.swift
@@ -0,0 +1,22 @@
+//
+//  MapView.swift
+//  Goalscorers
+//
+//  Created by tichinose1 on 2019/10/22.
+//  Copyright © 2019 tichinose1. All rights reserved.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: UIViewRepresentable {
+    typealias UIViewType = MKMapView
+
+    func makeUIView(context: Context) -> MKMapView {
+        MKMapView()
+    }
+
+    func updateUIView(_ uiView: MKMapView, context: Context) {
+        print(#function)
+    }
+}
