8bfc3c00eaba861ffbb0e81616c2cd13482a68ec
tichinose1 <tichinose1dev@gmail.com>
2019-10-24 22:57:30 +0900

    Support Players

--- Goalscorers/Views/Players/Players.swift
+++ Goalscorers/Views/Players/Players.swift
@@ -11,14 +11,42 @@ import Firebase
 
 struct Players: View {
     @State private var items: [QueryDocumentSnapshot] = []
+    @State private var isPresented: Bool = false
 
     var body: some View {
         NavigationView {
             List(items) { item in
-                PlayerRow(name: item.data()["name"] as! String)
+                Button(action: { self.isPresented = true }) {
+                    PlayerRow(name: item.data()["name"] as! String)
+                }
+                .sheet(isPresented: self.$isPresented) {
+                    SafariView(url: item.url)
+                }
             }
             .navigationBarTitle("Players")
         }
+        .onAppear(perform: self.onAppear)
+    }
+}
+
+private extension Players {
+
+    func onAppear() {
+        Firestore.firestore().collection("players").addSnapshotListener { snapshot, error in
+            if let error = error {
+                print("error: \(error)")
+                return
+            }
+            guard let snapshot = snapshot else { return }
+            self.items = snapshot.documents
+        }
+    }
+}
+
+private extension QueryDocumentSnapshot {
+
+    var url: URL {
+        URL(string: data()["url"] as! String)!
     }
 }
 
