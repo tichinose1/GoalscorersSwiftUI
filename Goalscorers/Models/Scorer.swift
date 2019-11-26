76a0834c7b015af153a6812b316db3db90137b2e
tichinose1 <tichinose1dev@gmail.com>
2019-11-27 01:05:53 +0900

    Add DocumentReference to models

--- Goalscorers/Models/Scorer.swift
+++ Goalscorers/Models/Scorer.swift
@@ -7,12 +7,23 @@
 //
 
 import Foundation
+import Firebase
+import FirebaseFirestoreSwift
 
 struct Scorer: Decodable {
     var url: URL
     var title: String
     var season: String
     var order: Int
+    var competitionRef: DocumentReference
+
+    private enum CodingKeys: String, CodingKey {
+        case url
+        case title
+        case season
+        case order
+        case competitionRef = "competition_ref"
+    }
 }
 
 extension Scorer: Identifiable {
@@ -24,9 +35,30 @@ extension Scorer: Identifiable {
 
 extension Scorer {
 
-    static var samples: [Scorer] {
-        return [
-            Scorer(url: URL(string: "example")!, title: "a", season: "2019", order: 0)
-        ]
+    static func fetchLatest(completion: @escaping (Result<[Scorer], GoalscorersError>) -> Void) {
+        Firestore
+            .firestore()
+            .collection("scorers")
+            .whereField("season", isGreaterThan: "2018")
+            .addSnapshotListener { snapshot, error in
+                var result: Result<[Scorer], GoalscorersError>
+                defer {
+                    completion(result)
+                }
+                if let error = error {
+                    result = .failure(.database(origin: error))
+                    return
+                }
+                guard let snapshot = snapshot else {
+                    result = .failure(.unknown)
+                    return
+                }
+                do {
+                    let items = try snapshot.documents.map { try $0.data(as: Scorer.self)! }
+                    result = .success(items)
+                } catch {
+                    result = .failure(.unknown)
+                }
+        }
     }
 }
