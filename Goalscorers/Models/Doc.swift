c1807f5e6d06d5623d81a2494123c2d6b7f1db2a
tichinose1 <tichinose1dev@gmail.com>
2019-11-30 23:22:50 +0900

    Add universal model wrapper

---
+++ Goalscorers/Models/Doc.swift
@@ -0,0 +1,18 @@
+//
+//  Doc.swift
+//  Goalscorers
+//
+//  Created by tichinose1 on 2019/11/30.
+//  Copyright © 2019 tichinose1. All rights reserved.
+//
+
+struct Doc<T: Decodable> {
+    var documentID: String
+    var data: T
+}
+
+extension Doc: Identifiable {
+    var id: String {
+        return documentID
+    }
+}
