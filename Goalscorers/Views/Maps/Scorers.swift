113ed98ccac8a3dd15113a270614761a18073a78
tichinose1 <tichinose1dev@gmail.com>
2019-12-03 22:45:15 +0900

    Add scorers view

---
+++ Goalscorers/Views/Maps/Scorers.swift
@@ -0,0 +1,21 @@
+//
+//  Scorers.swift
+//  Goalscorers
+//
+//  Created by tichinose1 on 2019/12/03.
+//  Copyright © 2019 tichinose1. All rights reserved.
+//
+
+import SwiftUI
+
+struct Scorers: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Scorers_Previews: PreviewProvider {
+    static var previews: some View {
+        Scorers()
+    }
+}
---
+++ Goalscorers/Views/Maps/ScorersRow.swift
@@ -0,0 +1,21 @@
+//
+//  ScorersRow.swift
+//  Goalscorers
+//
+//  Created by tichinose1 on 2019/12/03.
+//  Copyright © 2019 tichinose1. All rights reserved.
+//
+
+import SwiftUI
+
+struct ScorersRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ScorersRow_Previews: PreviewProvider {
+    static var previews: some View {
+        ScorersRow()
+    }
+}
